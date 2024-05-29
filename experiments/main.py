029bc74c23e92bb9f05bceb1aa46f2f9c05f9251
Ennio Visconti <ennio.visconti@gmail.com>
2024-05-29 20:07:35 +0200

    feat: main script to run all benchmarks

--- experiments/main.py
+++ experiments/main.py
@@ -10,7 +10,7 @@ logger = getLogger(__name__)
 dir = path.dirname(path.realpath(__file__))
 EXP_DIR = f"{dir}/../models/NN - Magnet"
 
-nn_config = Configuration(
+nn_config_1 = Configuration(
     exp_name="NN",
     exp_batch_name="TURBO",
     exp_instance_number=1,
@@ -28,11 +28,63 @@ nn_config = Configuration(
     optimization_upper_bounds=3.0,
 )
 
+nnx_config_1 = Configuration(
+    exp_name="NN",
+    exp_batch_name="TURBO",
+    exp_instance_number=1,
+    exp_repetitions=10,
+    optimization_iterations=72,
+    simulator_model_path=EXP_DIR,
+    simulator_repetitions=1,
+    # Experiment-specific
+    monitor_spec=f"{dir}/nn/spec_nnx.mls",
+    monitor_formula_name="NNx",
+    simulator_hyper_params={
+        'length': [35],
+    },
+    optimization_lower_bounds=1.95,
+    optimization_upper_bounds=2.05,
+)
+
+nn_config_2 = Configuration(
+    exp_name="NN",
+    exp_batch_name="TURBO",
+    exp_instance_number=2,
+    exp_repetitions=10,
+    optimization_iterations=10,
+    simulator_model_path=EXP_DIR,
+    simulator_repetitions=1,
+    # Experiment-specific
+    monitor_spec=f"{dir}/nn/spec_nn.mls",
+    monitor_formula_name="NN",
+    simulator_hyper_params={
+        'length': [3],
+    },
+    optimization_lower_bounds=1.0,
+    optimization_upper_bounds=3.0,
+)
+
+nnx_config_2 = Configuration(
+    exp_name="NN",
+    exp_batch_name="TURBO",
+    exp_instance_number=2,
+    exp_repetitions=10,
+    optimization_iterations=10,
+    simulator_model_path=EXP_DIR,
+    simulator_repetitions=1,
+    # Experiment-specific
+    monitor_spec=f"{dir}/nn/spec_nnx.mls",
+    monitor_formula_name="NNx",
+    simulator_hyper_params={
+        'length': [3],
+    },
+    optimization_lower_bounds=1.95,
+    optimization_upper_bounds=2.05,
+)
+
 
 def main():
-    logger.info(f"Running experiment {nn_config.exp_name}")
-
-    nn = NNRunner(nn_config)
+    nn = NNRunner(nn_config_2)
     nn.run_batch()
 
 
