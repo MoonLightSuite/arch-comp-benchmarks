d04343e68940bf62e7f8ccbed4102e01c4fe5d6f
Ennio Visconti <ennio.visconti@gmail.com>
2024-06-04 11:14:21 +0200

    chore: minor exp cleanup

--- experiments/nn/run_neural.m
+++ experiments/nn/run_neural.m
@@ -4,16 +4,10 @@ function [tout, yout, xin] = run_neural(u, T)
     
     tin = 0:0.01:T;
     xin = interp1(ts, us, tin, 'previous');
-    % disp(size(xin))
-    % disp(size(tin))
     u = [tin' xin'];
-
-    % disp(u)
-    % plot(u)
 
     assignin('base','u',u);
     assignin('base','T',T);
-
 
     load_system('narmamaglev_v1');
     
